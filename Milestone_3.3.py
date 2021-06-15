f909ca3062244b6bcaa5c953ce1ca2e7d9c6f924
Jared <37633862+JaredEngelken@users.noreply.github.com>
2021-06-15 17:24:46 +0200

    Update Milestone_3.3.py

--- Milestone_3.3.py
+++ Milestone_3.3.py
@@ -41,7 +41,7 @@ def equilibrium(rho, ux, uy):
     weights[7]*rho*(1 + 3*c7 + 9/2*c7**2 - 3/2*uu),
     weights[8]*rho*(1 + 3*c8 + 9/2*c8**2 - 3/2*uu)])
 
-def collide(f, omega):
+def collision(f, omega):
     rho = np.sum(f, axis=0)
     ux = (f[1] - f[3] + f[5] - f[6] - f[7] + f[8])/rho
     uy = (f[2] - f[4] + f[5] + f[6] - f[7] - f[8])/rho
@@ -87,7 +87,7 @@ uAnim = np.zeros((Nx, Ny, timestep))
 for om in omega:
     for i in range(timestep):
         streaming(f)
-        rho, ux_kl, uy_kl = collide(f, om)
+        rho, ux_kl, uy_kl = collision(f, om)
         rhoAnim[:,:,i] = rho
         uAnim[:,:,i] = uy_kl
         # Fourier analysis
